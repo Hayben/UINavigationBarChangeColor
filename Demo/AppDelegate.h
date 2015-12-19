e5dc336ab67f7030959940254dbd114ea3c7a887
123 <Hayben_gong@163.com>
2015-12-19 14:16:50 +0800

    Initial Commit

---
+++ Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Demo
+//
+//  Created by 123 on 15/12/19.
+//  Copyright © 2015年 haibin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
